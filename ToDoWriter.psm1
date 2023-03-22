f275fac63aa737e2f7cc3c80730fcc73ff1557d0
shtef21 <stjepan.salopek@hotmail.com>
2023-03-22 09:58:41 +0100

    Startup instructions

--- ToDoWriter.psm1
+++ ToDoWriter.psm1
@@ -10,8 +10,29 @@ function Start-ToDoClient {
     . ./client.ps1
 }
 
-# Export functions
-Export-ModuleMember -Function Start-ToDoClient, Show-ToDo, Add-ToDo, Set-ToDo, Reset-ToDo, Remove-ToDo 
+function Set-ToDoClientStartup {
+    # Set ToDoWriter client to run on startup
+
+    # Check if script has admin privileges
+    if ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544") -eq $false) {
+        Write-Host "Cannot set client startup. Please run script as admin."
+        return
+    }
+    
+    Write-Host "Determining startup directory path..."
+    $path = "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+    
+    Write-Host "To enable client startup, please go to..."
+    Write-Host "  $path"
+    Write-Host "and create a shortcut to..."
+    Write-Host "  $path\client.ps1"
+}
+
+# Export TODO functions
+Export-ModuleMember -Function Show-ToDo, Add-ToDo, Set-ToDo, Reset-ToDo, Remove-ToDo
+
+# Export management functions
+Export-ModuleMember -Function Start-ToDoClient, Set-ToDoClientStartup
 
 # Set aliases if not exist
 if ((Test-Path alias:*todo) -eq $false) {
